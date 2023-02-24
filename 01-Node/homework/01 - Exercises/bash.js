48703507833bd74de3711efd4a9b32695d77aead
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-02-24 12:31:30 -0300

    M3-Node-hw

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,7 +1,19 @@
+
 const process = require("process");
 const commands = require("./commands/index.js");
 
+function print(output) {
+  process.stdout.write(output);
+  process.stdout.write('\nprompt > ');
+}
+
 function bash() {
+  process.stdout.write('prompt > ');
+  process.stdin.on('data', (data) => {
+    let args = data.toString().trim().split(' ');
+    let cmd = args.shift();
+    commands[cmd] ? commands[cmd](print, args.join(' ')) : print(`command not found: ${cmd}`);
+  })
 }
 
 bash();
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,60 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
 
-function date() {}
+function pwd(print) {
+    return print(process.cwd());
+}
 
-function echo() {}
+function date(print) {
+    return print(Date());
+}
 
-function ls() {}
+function echo(print, args) {
+    return print(args);
+}
 
-function cat() {}
+function ls(print) {
+    fs.readdir('.', (error, files) => {
+        if(error) throw new Error(error);
+        print(files.join(' '));
+    });
+}
 
-function head() {}
+function cat(print, args) {
+   fs.readFile(args, 'utf-8', (error, data) => {
+    if(error) throw new Error(error);
+    print(data.split('\n').join('\n'));
+   })
+}
 
-function tail() {}
+function head(print, args) {
+    fs.readFile(args, 'utf-8', (error, data) => {
+        if(error) throw new Error(error);
+        print(data.split('\n')[0].trim());
+    });
+}
 
-function curl() {}
+function tail(print, args) {
+    fs.readFile(args, 'utf-8', (error, data) => {
+        if(error) throw new Error(error);
+        print(data.split('\n').pop().trim());
+    })
+}
 
-module.exports = {};
+function curl(print, args) {
+    utils.request(args, (error, response) => {
+        error ? console.log(error) : print(response);
+    })
+}
+
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail,
+    curl,
+};
