fe5385a9ff74eb32d086d25770148c95d2cd27a7
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-02-24 21:15:45 -0300

    M3-Web Server -hw 01

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -2,9 +2,38 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 3001;
+const server = http.createServer((req, res) => {
+  console.log(`Server raised in port ${PORT}`);
+  if(req.url === '/api') {
+    fs.readFile('utils/dogsData.json', 'utf-8', (err, data) => {
+      if(err) {
+        res.writeHead(404, { 'Content-type':'text/plain' });
+        res.end("json not found");
+      } else {
+        res.writeHead(200, { 'Content-type':'application/json'});
+        res.end(data); // porque no se usa JSON.stringify??? la lecture dice una cosa y la hw no la respeta...
+      }
+    });
+    return;
+  }
+  if(req.url === "/allDogs") {
+    fs.readFile('utils/allDogs.html', 'utf-8', (err, data) => {
+      if(err) {
+        res.writeHead(404, { 'Content-type': 'text/plain'});
+        res.end('html not found');
+      } else{
+        res.writeHead(200, { 'Content-type':'text/html' });
+        res.end(data);
+      }
+    })
+    return;
+  }
+  res.writeHead(404, { 'Content-type':'text/plain' });
+  res.end('Route not found')
+}).listen(PORT, 'localhost');
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
 module.exports =
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  null;
+  server;
